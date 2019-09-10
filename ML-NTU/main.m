aa12d7628966ba91b36ace050fea22040c4d091d
Sizhe Wei <sizhewei@sjtu.edu.cn>
2019-09-10 09:59:16 +0800

    read file

--- ML-NTU/main.m
+++ ML-NTU/main.m
@@ -1,1 +1,7 @@
-file = ''
+file = "data1.txt";
+fileid = fopen(file);
+dataread = textscan(fileid,'%f%f%f%f%d');
+data = cell2mat(dataread(1,1));
+% for i=1:size(data,2)
+%     data(i) = cell2mat(data(i));
+% end
